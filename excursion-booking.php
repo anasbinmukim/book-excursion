26ec7abf4df9e665773d473c9af4743d62e5ea50
Anas <anasbinmukim@gmail.com>
2017-11-02 23:16:37 +0600

    Start here

---
+++ excursion-booking.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Excursion Booking
+Plugin URI: http://plugins.rmweblab.com/excursion-booking/
+Description: Excursion Booking is short trip related booking system.
+Author: RM Web Lab
+Version: 1.0
+Author URI: https://rmweblab.com
+Copyright: © 2017 RM Web Lab.
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: excursion_booking
+Domain Path: /languages
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
